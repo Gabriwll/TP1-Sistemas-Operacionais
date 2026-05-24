8b8c3e2b3f7441255ac72bc787a22d871d0a1c9b
Francyelle <francyelle.serrano@ufv.br>
2026-05-24 13:36:42 -0300

    style: correção de identação e retirada de print teste

--- src/output/interface.c
+++ src/output/interface.c
@@ -13,42 +13,42 @@
 
 void print_dashboard(int current_time, CPU *cpu, Scheduler *scheduler,
                      Queue *blocked_queue, int modo_detalhado) {
-  system("clear");
+    system("clear");
 
-  printf(BOLD WHITE
-         "\n==================================================\n" RESET);
-  printf(BOLD WHITE
-         "              SISTEMA OPERACIONAL                 \n" RESET);
-  printf(BOLD WHITE
-         "==================================================\n" RESET);
-  printf(BOLD WHITE " Tempo atual: " BOLD YELLOW "%d\n" RESET, current_time);
-  if (cpu->current_process) {
-    printf(BOLD WHITE " Status CPU: " BOLD GREEN "OCUPADA\n" RESET);
-  } else {
-    printf(BOLD WHITE " Status CPU: " BOLD RED "OCIOSA\n" RESET);
-  }
-  printf(BOLD WHITE
-         "--------------------------------------------------\n" RESET);
-
-  if (cpu->current_process) {
-    printf(BOLD GREEN "[ PROCESSO EM EXECUCAO ]\n" RESET);
-    print_process(cpu->current_process, modo_detalhado);
-  }
-
-  printf(BOLD WHITE
-         "--------------------------------------------------\n" RESET);
-  printf(BOLD BLUE "[ FILAS DE PROCESSOS ]\n" RESET);
-
-  if (scheduler->type == SCHED_MLFQ) {
-    for (int i = 0; i < 4; i++) {
-      print_queue(&scheduler->readyQueues[i], current_time, modo_detalhado);
+    printf(BOLD WHITE
+           "\n==================================================\n" RESET);
+    printf(BOLD WHITE
+           "              SISTEMA OPERACIONAL                 \n" RESET);
+    printf(BOLD WHITE
+           "==================================================\n" RESET);
+    printf(BOLD WHITE " Tempo atual: " BOLD YELLOW "%d\n" RESET, current_time);
+    if (cpu->current_process) {
+      printf(BOLD WHITE " Status CPU: " BOLD GREEN "OCUPADA\n" RESET);
+    } else {
+      printf(BOLD WHITE " Status CPU: " BOLD RED "OCIOSA\n" RESET);
     }
-  } else {
-    print_queue(&scheduler->rrQueue, current_time, modo_detalhado);
-  }
+    printf(BOLD WHITE
+           "--------------------------------------------------\n" RESET);
 
-  print_queue(blocked_queue, current_time, modo_detalhado);
+    if (cpu->current_process) {
+      printf(BOLD GREEN "[ PROCESSO EM EXECUCAO ]\n" RESET);
+      print_process(cpu->current_process, modo_detalhado);
+    }
 
-  printf(BOLD WHITE
-         "==================================================\n\n" RESET);
+    printf(BOLD WHITE
+           "--------------------------------------------------\n" RESET);
+    printf(BOLD BLUE "[ FILAS DE PROCESSOS ]\n" RESET);
+
+    if (scheduler->type == SCHED_MLFQ) {
+        for (int i = 0; i < 4; i++) {
+            print_queue(&scheduler->readyQueues[i], current_time, modo_detalhado);
+        }
+    } else {
+        print_queue(&scheduler->rrQueue, current_time, modo_detalhado);
+    }
+
+    print_queue(blocked_queue, current_time, modo_detalhado);
+
+    printf(BOLD WHITE
+           "==================================================\n\n" RESET);
 }
--- src/output/printer.c
+++ src/output/printer.c
@@ -6,28 +6,27 @@
 #include <unistd.h>
 
 void spawn_print_process(pid_t *print_pid, int current_time, CPU *cpu,
-                         Scheduler *scheduler, Queue *blocked_queue,
+                          Scheduler *scheduler, Queue *blocked_queue,
                          int modo_detalhado) {
-  fprintf(stderr,"DEBUG Tentando criar processo de impressao...\n");
-  // verifica se o processo anterior ainda está vivo
-  if (*print_pid > 0) {
-    int status;
-    // WNOHANG impede que o pai fique parado esperando
-    if (waitpid(*print_pid, &status, WNOHANG) == 0) {
-      printf("[GERENCIADOR] Aviso: Impressao em curso, aguarde...\n");
-      return;
+    // verifica se o processo anterior ainda está vivo
+    if (*print_pid > 0) {
+        int status;
+        // WNOHANG impede que o pai fique parado esperando
+        if (waitpid(*print_pid, &status, WNOHANG) == 0) {
+            printf("[GERENCIADOR] Aviso: Impressao em curso, aguarde...\n");
+            return;
+        }
     }
-  }
 
-  *print_pid = fork();
+    *print_pid = fork();
 
-  if (*print_pid < 0) {
-    perror("Erro ao criar processo de impressao");
-  } else if (*print_pid == 0) {
-    // filho chama a interface e morre
-    print_dashboard(current_time, cpu, scheduler, blocked_queue,
+    if (*print_pid < 0) {
+        perror("Erro ao criar processo de impressao");
+    } else if (*print_pid == 0) {
+        // filho chama a interface e morre
+        print_dashboard(current_time, cpu, scheduler, blocked_queue,
                     modo_detalhado);
-    exit(0);
-  }
-  // pai retorna para continuar a simulação
+        exit(0);
+    }
+    // pai retorna para continuar a simulação
 }
